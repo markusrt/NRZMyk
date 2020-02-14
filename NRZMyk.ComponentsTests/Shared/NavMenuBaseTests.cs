83e71f22cca7c1db294e2825aea3e5a82de2e279
Markus Reinhardt <tuxxilla@gmail.com>
2020-02-15 00:27:39 +0100

    Create initial project structure
    Based on https://www.c-sharpcorner.com/article/future-ready-blazor-application-architecture/
    with some modifications. E.g. add service project for all services (database, external apis, ...).

---
+++ NRZMyk.ComponentsTests/Shared/NavMenuBaseTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NRZMyk.Components.Shared;
+using NUnit.Framework;
+
+namespace NRZMyk.ComponentsTests.Shared
+{
+    class NavMenuBaseTests
+    {
+        [Test]
+        public void WhenCreated_ThenMenuIsCollapsed()
+        {
+            var sut = new NavMenuBase();
+
+            sut.NavMenuCssClass.Should().Be("collapse");
+        }
+
+        [Test]
+        public void WhenToggle_ThenMenuCssClassChanges()
+        {
+            var sut = new NavMenuBase();
+
+            sut.ToggleNavMenu();
+
+            sut.NavMenuCssClass.Should().BeNull();
+        }
+    }
+}
---
+++ NRZMyk.Server/_Host.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NRZMyk.Server
+{
+    public class _HostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
