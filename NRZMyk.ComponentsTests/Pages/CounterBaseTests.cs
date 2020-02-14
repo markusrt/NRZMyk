47d75a8507e72220d6d95072dd700314d878c1d5
Markus Reinhardt <tuxxilla@gmail.com>
2020-02-15 00:27:39 +0100

    Create initial project structure
    Based on https://www.c-sharpcorner.com/article/future-ready-blazor-application-architecture/
    with some modifications. E.g. add service project for all services (database, external apis, ...).

---
+++ NRZMyk.ComponentsTests/Pages/CounterBaseTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NRZMyk.Components.Pages;
+using NUnit.Framework;
+
+namespace NRZMyk.ComponentsTests.Pages
+{
+    public class CounterBaseTests
+    {
+        [Test]
+        public void WhenCreated_ThenCountIsZero()
+        {
+            var sut = new CounterBase();
+
+            sut.CurrentCount.Should().Be(0);
+        }
+
+        [Test]
+        public void WhenIncrementCount_ThenCountIsIncreased()
+        {
+            var sut = new CounterBase();
+
+            sut.IncrementCount();
+
+            sut.CurrentCount.Should().Be(1);
+        }
+    }
+}
