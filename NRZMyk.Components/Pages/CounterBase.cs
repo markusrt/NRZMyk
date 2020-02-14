c3ec1e63587d28c4e65b675c257c84fbd030ef3a
Markus Reinhardt <--help>
2020-02-15 00:27:39 +0100

    Create initial project structure
    Based on https://www.c-sharpcorner.com/article/future-ready-blazor-application-architecture/
    with some modifications. E.g. add service project for all services (database, external apis, ...).

---
+++ NRZMyk.Components/Pages/CounterBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NRZMyk.Components.Pages
+{
+    public class CounterBase : ComponentBase
+    {
+        public int CurrentCount { get; set; }
+
+        public void IncrementCount()
+        {
+            CurrentCount++;
+        }
+    }
+}
---
+++ NRZMyk.Components/Shared/NavMenuBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace NRZMyk.Components.Shared
+{
+    public class NavMenuBase : ComponentBase
+    {
+        private bool collapseNavMenu = true;
+
+        public string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        public void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+    }
+}
