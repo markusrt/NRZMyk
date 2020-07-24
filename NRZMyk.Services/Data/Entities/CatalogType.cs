9d0a30566cc5e4302d9c3e7daf640f8ffa250524
Markus Reinhardt <tuxxilla@gmail.com>
2020-07-25 00:50:57 +0200

    Implement basic repository structure
    CatalogItem is just used for POC. To be replaced by corresponding
    Sentinel Data entity later.
    
    Based on <https://github.com/dotnet-architecture/eShopOnWeb>
    
    #2

---
+++ NRZMyk.Services/Data/Entities/CatalogType.cs
@@ -0,0 +1,13 @@
+﻿using NRZMyk.Services.Interfaces;
+
+namespace NRZMyk.Services.Data.Entities
+{
+    public class CatalogType : BaseEntity, IAggregateRoot
+    {
+        public string Type { get; private set; }
+        public CatalogType(string type)
+        {
+            Type = type;
+        }
+    }
+}
