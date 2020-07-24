6221f526f76bc850f6f5a258c3ae06e17c804c59
Markus Reinhardt <--help>
2020-07-24 17:08:29 +0200

    #1 Integrate AADB2C
    Based on this documentation
    https://docs.microsoft.com/en-US/aspnet/core/blazor/security/webassembly/hosted-with-azure-active-directory-b2c?view=aspnetcore-3.1
    
    Merge resulting structure to existing project

---
+++ NRZMyk.Server/Model/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NRZMyk.Server.Model
+{
+    [Flags]
+    public enum Role
+    {
+        None = 0,
+        Guest = 1,
+        User = 2,
+        Admin = 4,
+        SuperUser = 8
+    }
+}
