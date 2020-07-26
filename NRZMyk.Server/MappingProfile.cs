c3d297985c36c980f0c0f619b074a3029f7a7148
Markus Reinhardt <tuxxilla@gmail.com>
2020-07-26 13:44:51 +0200

    Restructure and combine duplicate Dto object #2

--- NRZMyk.Server/MappingProfile.cs
+++ NRZMyk.Server/MappingProfile.cs
@@ -3,6 +3,7 @@ using NRZMyk.Server.Controllers.CatalogBrands;
 using NRZMyk.Server.Controllers.CatalogItems;
 using NRZMyk.Server.Controllers.CatalogTypes;
 using NRZMyk.Services.Data.Entities;
+using NRZMyk.Services.Models;
 
 namespace NRZMyk.Server
 {
