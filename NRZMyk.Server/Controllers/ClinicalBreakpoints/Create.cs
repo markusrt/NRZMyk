5e9bcd0308c600d0f7f8b8e97f5245c7e759a01b
Markus Reinhardt <tuxxilla@gmail.com>
2020-08-04 22:24:19 +0200

    Add controller for clinical breakpoints
    Web API controller to list and create breakpoints #2

---
+++ NRZMyk.Server/Controllers/ClinicalBreakpoints/Create.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Ardalis.ApiEndpoints;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NRZMyk.Services.Data.Entities;
+using NRZMyk.Services.Interfaces;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace NRZMyk.Server.Controllers.ClinicalBreakpoints
+{
+    public class Create : BaseAsyncEndpoint<CreateClinicalBreakpointRequest, ClinicalBreakpoint>
+    {
+        private readonly IAsyncRepository<ClinicalBreakpoint> _clinicalBreakpointRepository;
+        private readonly IMapper _mapper;
+
+        public Create(IAsyncRepository<ClinicalBreakpoint> clinicalBreakpointRepository, IMapper mapper)
+        {
+            _clinicalBreakpointRepository = clinicalBreakpointRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost("api/clinical-breakpoints")]
+        [SwaggerOperation(
+            Summary = "Creates a new Clinical Breakpoint",
+            OperationId = "clinical-breakpoints.create",
+            Tags = new[] { "SharedEndpoints" })
+        ]
+        public override async Task<ActionResult<ClinicalBreakpoint>> HandleAsync(CreateClinicalBreakpointRequest request)
+        {
+            var newEntry = _mapper.Map<ClinicalBreakpoint>(request);
+            return await _clinicalBreakpointRepository.AddAsync(newEntry);
+        }
+    }
+}
---
+++ NRZMyk.Server/Controllers/ClinicalBreakpoints/List.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ardalis.ApiEndpoints;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NRZMyk.Services.Data.Entities;
+using NRZMyk.Services.Interfaces;
+using NRZMyk.Services.Specifications;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace NRZMyk.Server.Controllers.ClinicalBreakpoints
+{
+    public class List : BaseAsyncEndpoint<ListClinicalBreakpointsRequest, List<ClinicalBreakpoint>>
+    {
+        private readonly IAsyncRepository<ClinicalBreakpoint> _clinicalBreakpointRepository;
+
+        public List(IAsyncRepository<ClinicalBreakpoint> clinicalBreakpointRepository)
+        {
+            _clinicalBreakpointRepository = clinicalBreakpointRepository;
+        }
+
+        [HttpGet("api/clinical-breakpoints")]
+        [SwaggerOperation(
+            Summary = "List Clinical Breakpoints",
+            OperationId = "clinical-breakpoints.list",
+            Tags = new[] { "SharedEndpoints" })
+        ]
+        public override async Task<ActionResult<List<ClinicalBreakpoint>>> HandleAsync([FromQuery]ListClinicalBreakpointsRequest request)
+        {
+            var filter = new ClinicalBreakpointFilterSpecification(request.Species);
+            var items = await _clinicalBreakpointRepository.ListAsync(filter);
+            return Ok(items);
+        }
+    }
+}
