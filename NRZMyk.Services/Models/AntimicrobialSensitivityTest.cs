b471d98d1298177171a9f72b0b4ce3a932b5ae07
Markus Reinhardt <tuxxilla@gmail.com>
2020-09-03 23:04:38 +0200

    Add first part of antrimicobial sensitivity editor
    #2

---
+++ NRZMyk.Services/Models/AntimicrobialSensitivityTest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using NRZMyk.Services.Data.Entities;
+
+namespace NRZMyk.Services.Models
+{
+    public class AntimicrobialSensitivityTest
+    {
+        public SpeciesTestingMethod TestingMethod { get; set; }
+
+        public ClinicalBreakpointReference EucastClinicalBreakpoint { get; set; }
+
+        public float  MinimumInhibitoryConcentration  { get; set; }
+
+        public Resistance Resistance { get; set; }
+    }
+}
