539502ef699b874092a5da190e8b2c7ec5569841
Markus Reinhardt <tuxxilla@gmail.com>
2020-09-27 14:34:35 +0200

    Integration of export API and frontend
    - API to export all sentinel entries to excel
    - Draft implementation for excel content
    - Client side integration of SaveFile JS interop
    
    #20

---
+++ NRZMyk.Components/Pages/SentinelEntryPage/ExportFilterBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using NRZMyk.Services.Services;
+
+namespace NRZMyk.Components.Pages.SentinelEntryPage
+{
+    public class ExportFilterBase : ComponentBase
+    {
+        [Inject]
+        internal IJSRuntime JsRuntime { get; set; }   
+
+        [Inject]
+        internal  SentinelEntryService SentinelEntryService { get; set; }
+
+        internal int IsDownloadStarted { get; private set; }
+        
+        protected async Task DownloadFile()
+        {
+            IsDownloadStarted = 1;
+            var fileData = await SentinelEntryService.Export();
+            var fileName =  $"Sentinel-Export_{DateTime.Now:yyyyMMdd}.xlsx";
+            await JsRuntime.InvokeAsync<object>("saveAsFile", new object[] { fileName, fileData });
+            IsDownloadStarted = 2;
+        }
+    }
+}
