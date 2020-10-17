821450ae49ff6581a7eb22c52a58d1629bcd8ee9
Markus Reinhardt <tuxxilla@gmail.com>
2020-10-17 20:41:46 +0200

    Create component for enum selection
    #38

---
+++ NRZMyk.Components/SharedComponents/Input/SelectEnumBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace NRZMyk.Components.SharedComponents.Input
+{
+    public class SelectEnumBase<T> : InputSelect<T>
+    {
+        [Parameter] public string Label { get; set; }
+        [Parameter] public Expression<Func<T>> ValidationFor { get; set; }
+        [Parameter] public bool ShowDefaultOption { get; set; } = true;
+        [Parameter] public bool AddFormRow { get; set; } = true;
+        [Parameter] public string SelectClass { get; set; } = "col-sm-6";
+    }
+}
