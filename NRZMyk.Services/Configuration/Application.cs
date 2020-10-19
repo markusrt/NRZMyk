b7ae57f8ddae67c51d1e23041b7ff4f7a68896eb
Markus Reinhardt <tuxxilla@gmail.com>
2020-10-19 08:10:22 +0200

    Add predefined list of other species
    #15

--- NRZMyk.Services/Configuration/Application.cs
+++ NRZMyk.Services/Configuration/Application.cs
@@ -1,7 +1,10 @@
-﻿namespace NRZMyk.Services.Configuration
+﻿using System.Collections.Generic;
+
+namespace NRZMyk.Services.Configuration
 {
     public class Application
     {
         public int CryoBoxSize { get; set; }
+        public List<string> OtherSpecies { get; set; }
     }
 }
