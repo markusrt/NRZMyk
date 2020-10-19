fd95fe40ca3651350359afe53dd341a758039f60
Markus Reinhardt <tuxxilla@gmail.com>
2020-10-19 19:40:18 +0200

    Add PCR details and configure editor/list/export
    #14

---
+++ NRZMyk.Server/Controllers/SentinelEntries/OtherPcrDetails.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using NRZMyk.Services.Configuration;
+using NRZMyk.Services.Data;
+using NRZMyk.Services.Models;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace NRZMyk.Server.Controllers.SentinelEntries
+{
+    [Authorize(Roles = nameof(Role.User))]
+    public class OtherPcrDetails : BaseAsyncEndpoint<List<string>>
+    {
+        private readonly ISentinelEntryRepository _sentinelEntryRepository;
+
+        public OtherPcrDetails(ISentinelEntryRepository sentinelEntryRepository, IOptions<ApplicationSettings> config)
+        {
+            _sentinelEntryRepository = sentinelEntryRepository;
+        }
+
+        [HttpGet("api/sentinel-entries/other/pcr-details")]
+        [SwaggerOperation(
+            Summary = "List all used other species)",
+            OperationId = "sentinel-entries.OtherPcrDetails",
+            Tags = new[] { "SentinelEndpoints" })
+        ]
+        public override async Task<ActionResult<List<string>>> HandleAsync()
+        {
+            var otherPcrDetails =  await _sentinelEntryRepository.Other(s => s.PcrDetails);
+            return Ok(otherPcrDetails);
+        }
+    }
+}
---
+++ NRZMyk.Services/Data/Migrations/20201019173415_SentinelEntry_PcrDetails.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NRZMyk.Services.Data.Migrations
+{
+    public partial class SentinelEntry_PcrDetails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PcrDetails",
+                table: "SentinelEntries",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PcrDetails",
+                table: "SentinelEntries");
+        }
+    }
+}
