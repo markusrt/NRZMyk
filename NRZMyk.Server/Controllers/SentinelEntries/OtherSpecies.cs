acb18f0c65945c621f05f36b02c6a240301354e6
Markus Reinhardt <tuxxilla@gmail.com>
2020-10-19 08:10:22 +0200

    Add predefined list of other species
    #15

--- NRZMyk.Server/Controllers/SentinelEntries/OtherSpecies.cs
+++ NRZMyk.Server/Controllers/SentinelEntries/OtherSpecies.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Ardalis.ApiEndpoints;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using NRZMyk.Services.Configuration;
 using NRZMyk.Services.Data;
-using NRZMyk.Services.Data.Entities;
-using NRZMyk.Services.Interfaces;
 using NRZMyk.Services.Models;
 using Swashbuckle.AspNetCore.Annotations;
 
@@ -16,10 +17,12 @@ namespace NRZMyk.Server.Controllers.SentinelEntries
     public class OtherSpecies : BaseAsyncEndpoint<List<string>>
     {
         private readonly ISentinelEntryRepository _sentinelEntryRepository;
+        private readonly List<string> _configuredOtherSpecies;
 
-        public OtherSpecies(ISentinelEntryRepository sentinelEntryRepository)
+        public OtherSpecies(ISentinelEntryRepository sentinelEntryRepository, IOptions<ApplicationSettings> config)
         {
             _sentinelEntryRepository = sentinelEntryRepository;
+            _configuredOtherSpecies = config?.Value?.Application?.OtherSpecies ?? new List<string>();
         }
 
         [HttpGet("api/sentinel-entries/other/species")]
@@ -30,8 +33,8 @@ namespace NRZMyk.Server.Controllers.SentinelEntries
         ]
         public override async Task<ActionResult<List<string>>> HandleAsync()
         {
-            var otherMaterials =  await _sentinelEntryRepository.Other(s => s.OtherIdentifiedSpecies);
-            return Ok(otherMaterials);
+            var otherSpecies =  await _sentinelEntryRepository.Other(s => s.OtherIdentifiedSpecies);
+            return Ok(otherSpecies.Union(_configuredOtherSpecies).Distinct().OrderBy(s=>s));
         }
     }
 }
