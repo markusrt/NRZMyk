a2d324e422f89e5ebdef429282be1a6a1e215d80
Markus Reinhardt <tuxxilla@gmail.com>
2021-01-15 06:02:21 +0100

    Add missing property to CryoBox unique index
    Closes #63

---
+++ NRZMyk.Services/Data/Migrations/20210115044103_AddMissingYearToCryoBoxIndex.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NRZMyk.Services.Data.Migrations
+{
+    public partial class AddMissingYearToCryoBoxIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SentinelEntries_CryoBoxNumber_CryoBoxSlot",
+                table: "SentinelEntries");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SentinelEntries_Year_CryoBoxNumber_CryoBoxSlot",
+                table: "SentinelEntries",
+                columns: new[] { "Year", "CryoBoxNumber", "CryoBoxSlot" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SentinelEntries_Year_CryoBoxNumber_CryoBoxSlot",
+                table: "SentinelEntries");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SentinelEntries_CryoBoxNumber_CryoBoxSlot",
+                table: "SentinelEntries",
+                columns: new[] { "CryoBoxNumber", "CryoBoxSlot" },
+                unique: true);
+        }
+    }
+}
