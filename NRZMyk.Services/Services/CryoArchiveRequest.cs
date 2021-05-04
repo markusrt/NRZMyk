67fb64305740562c384e101f899f8e35e291db58
Markus Reinhardt <tuxxilla@gmail.com>
2021-05-04 19:03:30 +0200

    Add controller and request for cryo archivation
    #43

---
+++ NRZMyk.Services/Services/CryoArchiveRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using NRZMyk.Services.Data.Entities;
+using NRZMyk.Services.Models;
+using NRZMyk.Services.Validation;
+
+namespace NRZMyk.Services.Services
+{
+    public class CryoArchiveRequest
+    {
+        public int Id { get; set; }
+
+        public DateTime? CryoDate { get; set; }
+
+        public string CryoRemark { get; set; }
+    }
+}
