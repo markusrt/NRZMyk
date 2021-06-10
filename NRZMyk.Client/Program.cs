3045570a97e1934576757dbf6603e0ede6af3bba
Markus Reinhardt <tuxxilla@gmail.com>
2021-06-10 23:48:30 +0200

    Update to latest dotnet 6 template for client app
    - Migrate Bootstrap 4 to 5
    - Move styles to shared css files
    - Use updated login flow (including nice password reset)

--- NRZMyk.Client/Program.cs
+++ NRZMyk.Client/Program.cs
@@ -1,75 +1,44 @@
-using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using NRZMyk.Client;
+using NRZMyk.Services.Configuration;
+using NRZMyk.Services.Services;
 using AutoMapper;
 using BlazorApplicationInsights;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Authentication.WebAssembly.Msal.Models;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using NRZMyk.Components;
-using NRZMyk.Services.Configuration;
 using NRZMyk.Services.Interfaces;
-using NRZMyk.Services.Services;
 
-namespace NRZMyk.Client
-{
-    public static class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-            RedirectToBaseUrlOnCriticalException(builder);
-
-            builder.Services.AddBlazorApplicationInsights();
-            builder.RootComponents.Add<App>("app");
-            builder.Services.AddHttpClient("NRZMyk.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<NRZMyk.Components.App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddHttpClient("NRZMyk.ServerAPI", 
+    client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
+    .CreateClient("NRZMyk.ServerAPI"));
 
-            builder.Services.AddAutoMapper(typeof(SentinelEntryService).Assembly);
+// Custom services start
 
-            // Supply HttpClient instances that include access tokens when making requests to the server project
-            builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("NRZMyk.ServerAPI"));
+builder.Services.AddBlazorApplicationInsights();
+builder.Services.AddAutoMapper(typeof(SentinelEntryService).Assembly);
+builder.Services.AddTransient<SentinelEntryService, SentinelEntryServiceImpl>();
+builder.Services.AddTransient<IAccountService, AccountService>();
+builder.Services.AddTransient<IClinicalBreakpointService, ClinicalBreakpointService>();
+builder.Services.AddTransient<IMicStepsService, MicStepsService>();
 
-            builder.Services.AddTransient<SentinelEntryService, SentinelEntryServiceImpl>();
-            builder.Services.AddTransient<IAccountService, AccountService>();
-            builder.Services.AddTransient<IClinicalBreakpointService, ClinicalBreakpointService>();
-            builder.Services.AddTransient<IMicStepsService, MicStepsService>();
+builder.Services.Configure<BreakpointSettings>(options =>
+{
+    builder.Configuration.Bind(options);
+});
 
-            builder.Services.Configure<BreakpointSettings>(options =>
-            {
-                builder.Configuration.Bind(options);
-            });
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://nrcmycosis.onmicrosoft.com/nrzmyk-client/API.Access");
+}).AddAccountClaimsPrincipalFactory<RemoteAuthenticationState, RemoteUserAccount, CustomUserFactory>();
 
-            builder.Services.AddMsalAuthentication(options =>
-            {
-                builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
-                options.ProviderOptions.DefaultAccessTokenScopes.Add("https://nrcmycosis.onmicrosoft.com/e7562337-a4df-40c3-a8a1-7cc33d6bc193/API.Access");
-            }).AddAccountClaimsPrincipalFactory<RemoteAuthenticationState, RemoteUserAccount,
-                CustomUserFactory>();
+// Custom services end
 
-            await builder.Build().RunAsync();
-        }
-
-        private static void RedirectToBaseUrlOnCriticalException(WebAssemblyHostBuilder builder)
-        {
-            //TODO check after blazor .net 5.0 release if the issue with password reset (see #35) is eventually adressed
-            //     This would allow to remove this workaround
-
-            var navigationManager = builder.Services.Single(
-                s => s.ServiceType == typeof(NavigationManager)).ImplementationInstance as NavigationManager;
-            var customLoggerProvider = new ReloadOnCriticalErrorLogProvider(navigationManager, "Unexpected error in authentication");
-            builder.Logging.AddProvider(customLoggerProvider);
-        }
-    }
-}
+await builder.Build().RunAsync();
--- NRZMyk.Component.Playground/NullApplicationInsights.cs
+++ NRZMyk.Component.Playground/NullApplicationInsights.cs
@@ -1,64 +1,109 @@
-﻿using BlazorApplicationInsights;
+﻿using System;
+using BlazorApplicationInsights;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.JSInterop;
 
 namespace NRZMyk.Components.Playground
 {
     internal class NullApplicationInsights : IApplicationInsights
     {
+        public Task InitBlazorApplicationInsightsAsync(IJSRuntime jSRuntime)
+        {
+             return Task.CompletedTask;
+        }
+
+        public Task TrackEvent(string name, Dictionary<string, object?>? properties = null)
+        {
+             return Task.CompletedTask;
+        }
+
+        public Task TrackTrace(string message, SeverityLevel? severityLevel = null, Dictionary<string, object?>? properties = null)
+        {
+             return Task.CompletedTask;
+        }
+
+        public Task TrackException(Error exception, string? id = null, SeverityLevel? severityLevel = null,
+            Dictionary<string, object?>? properties = null)
+        {
+             return Task.CompletedTask;
+        }
+
+        public Task TrackPageView(string? name = null, string? uri = null, string? refUri = null, string? pageType = null,
+            bool? isLoggedIn = null, Dictionary<string, object?>? properties = null)
+        {
+             return Task.CompletedTask;
+        }
+
+        public Task StartTrackPage(string? name = null)
+        {
+             return Task.CompletedTask;
+        }
+
+        public Task StopTrackPage(string? name = null, string? url = null, Dictionary<string, string?>? properties = null,
+            Dictionary<string, decimal>? measurements = null)
+        {
+             return Task.CompletedTask;
+        }
+
+        public Task TrackMetric(string name, double average, double? sampleCount = null, double? min = null, double? max = null,
+            Dictionary<string, object?>? properties = null)
+        {
+             return Task.CompletedTask;
+        }
+
+        public Task TrackDependencyData(string id, string name, decimal? duration = null, bool? success = null,
+            DateTime? startTime = null, int? responseCode = null, string? correlationContext = null, string? type = null,
+            string? data = null, string? target = null)
+        {
+             return Task.CompletedTask;
+        }
+
+        public Task Flush(bool? async)
+        {
+             return Task.CompletedTask;
+        }
+
         public Task ClearAuthenticatedUserContext()
         {
-            return Task.CompletedTask;
+             return Task.CompletedTask;
         }
 
-        public Task Flush(bool? async = true)
+        public Task SetAuthenticatedUserContext(string authenticatedUserId, string? accountId = null, bool storeInCookie = false)
         {
-            return Task.CompletedTask;
+             return Task.CompletedTask;
         }
 
-        public Task SetAuthenticatedUserContext(string authenticatedUserId, string accountId = null, bool storeInCookie = false)
+        public Task AddTelemetryInitializer(TelemetryItem telemetryItem)
         {
-            return Task.CompletedTask;
+             return Task.CompletedTask;
         }
 
-        public Task StartTrackPage(string name = null)
+        public Task TrackPageViewPerformance(PageViewPerformanceTelemetry pageViewPerformance)
         {
-            return Task.CompletedTask;
+             return Task.CompletedTask;
         }
 
-        public Task StopTrackPage(string name = null, string url = null)
+        public Task StartTrackEvent(string name)
         {
-            return Task.CompletedTask;
+             return Task.CompletedTask;
         }
 
-        public Task TrackDependencyData(string id, double responseCode, string absoluteUrl = null, bool? success = null, string commandName = null, double? duration = null, string method = null, Dictionary<string, object> properties = null)
+        public Task StopTrackEvent(string name, Dictionary<string, string?>? properties = null, Dictionary<string, decimal>? measurements = null)
         {
-            return Task.CompletedTask;
+             return Task.CompletedTask;
         }
 
-        public Task TrackEvent(string name, Dictionary<string, object> properties = null)
+        public Task SetInstrumentationKey(string key)
         {
-            return Task.CompletedTask;
+             return Task.CompletedTask;
         }
 
-        public Task TrackException(Error error, SeverityLevel? severityLevel = null, Dictionary<string, object> properties = null)
+        public Task LoadAppInsights()
         {
-            return Task.CompletedTask;
+             return Task.CompletedTask;
         }
 
-        public Task TrackMetric(string name, double average, double? sampleCount = null, double? min = null, double? max = null, Dictionary<string, object> properties = null)
-        {
-            return Task.CompletedTask;
-        }
-
-        public Task TrackPageView(string name = null, string uri = null, string refUri = null, string pageType = null, bool? isLoggedIn = null, Dictionary<string, object> properties = null)
-        {
-            return Task.CompletedTask;
-        }
-
-        public Task TrackTrace(string message, SeverityLevel? severityLevel = null, Dictionary<string, object> properties = null)
-        {
-            return Task.CompletedTask;
-        }
+        public bool EnableAutoRouteTracking { get; set; }
     }
 }
