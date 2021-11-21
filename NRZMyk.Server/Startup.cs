6f4f2370e25bae3c1fe55fa99bd74991b561e20f
Markus Reinhardt <tuxxilla@gmail.com>
2021-11-21 20:53:54 +0100

    Fix major sonar issues

--- NRZMyk.Server/Startup.cs
+++ NRZMyk.Server/Startup.cs
@@ -71,7 +71,6 @@ namespace NRZMyk.Server
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddApplicationInsightsTelemetry();
 
-            services.AddSingleton<WeatherForecastService>();
             services.AddScoped(typeof(IAsyncRepository<>), typeof(EfRepository<>));
             services.AddScoped<ISentinelEntryRepository, SentinelEntryRepository>();
             services.AddScoped<IProtectKeyToOrganizationResolver, ProtectKeyToOrganizationResolver>();
