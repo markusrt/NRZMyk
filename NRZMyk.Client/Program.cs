87ba9e5664014914ccf29f5695b72ab671d91701
Markus Reinhardt <tuxxilla@gmail.com>
2021-11-23 07:21:32 +0100

    Change to updated client id

--- NRZMyk.Client/Program.cs
+++ NRZMyk.Client/Program.cs
@@ -54,10 +54,10 @@ namespace NRZMyk.Client
             builder.Services.AddMsalAuthentication(options =>
             {
                 builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
-                options.ProviderOptions.DefaultAccessTokenScopes.Add("https://nrcmycosis.onmicrosoft.com/nrzmyk-client/API.Access");
+                options.ProviderOptions.DefaultAccessTokenScopes.Add("https://nrcmycosis.onmicrosoft.com/e7562337-a4df-40c3-a8a1-7cc33d6bc193/API.Access");
             }).AddAccountClaimsPrincipalFactory<RemoteAuthenticationState, RemoteUserAccount,
                 CustomUserFactory>();
-
+            
             await builder.Build().RunAsync();
         }
 
