d6e10981928af3c3ae235095f8e8ea2b12fa1199
Markus Reinhardt <tuxxilla@gmail.com>
2022-02-12 11:47:09 +0100

    Ensure migrations are building correctly

---
+++ NRZMyk.Services.Tests/Data/Migrations/MigrationsTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.DependencyInjection;
+using NRZMyk.Services.Data;
+using NRZMyk.Services.Data.Migrations;
+using NUnit.Framework;
+
+namespace NRZMyk.Services.Tests.Data.Migrations;
+
+public class MigrationsTests
+{
+    [TestCaseSource(nameof(Migrations))]
+    public void WhenUpOperationsAreBuilt_DoesNotCrash(Type migration)
+    {
+        var sut = Activator.CreateInstance(migration) as Migration;
+
+        sut.Should().NotBeNull();
+        sut!.UpOperations.Should().NotBeEmpty();
+    }
+
+    [TestCaseSource(nameof(Migrations))]
+    public void WhenDownOperationsAreBuilt_DoesNotCrash(Type migration)
+    {
+        var sut = Activator.CreateInstance(migration) as Migration;
+
+        sut.Should().NotBeNull();
+        sut!.DownOperations.Should().NotBeEmpty();
+    }
+
+    [TestCaseSource(nameof(Migrations))]
+    public void WhenTargetModelIsBuilt_DoesNotCrash(Type migration)
+    {
+        var sut = Activator.CreateInstance(migration) as Migration;
+
+        sut.Should().NotBeNull();
+        sut!.TargetModel.ToDebugString().Should().NotBeEmpty();
+    }
+
+    private static IEnumerable<Type> Migrations()
+      => typeof(ApplicationDbContext).Assembly.GetTypes().Where(t => typeof(Migration).IsAssignableFrom(t));
+}
