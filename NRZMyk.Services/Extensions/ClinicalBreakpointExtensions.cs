b52b0a9b68596ecc999a48e530658895db0d36d7
Markus Reinhardt <tuxxilla@gmail.com>
2023-06-04 12:05:37 +0200

    Move some logic to extension method

---
+++ NRZMyk.Services/Extensions/ClinicalBreakpointExtensions.cs
@@ -0,0 +1,21 @@
+﻿using NRZMyk.Services.Data.Entities;
+
+namespace NRZMyk.Services.Extensions;
+
+public static class ClinicalBreakpointExtensions
+{
+    public static bool IsResistantAccordingToEucastDefinition(this float? mic, ClinicalBreakpoint breakpoint)
+    {
+        return mic > breakpoint.MicBreakpointResistent && breakpoint.Standard == BrothMicrodilutionStandard.Eucast;
+    }
+
+    public static bool IsResistantAccordingToClsiDefinition(this float? mic, ClinicalBreakpoint breakpoint)
+    {
+        return mic >= breakpoint.MicBreakpointResistent && breakpoint.Standard == BrothMicrodilutionStandard.Clsi;
+    }
+
+    public static bool IsSusceptibleAccordingToBothDefinitions(this float? mic, ClinicalBreakpoint breakpoint)
+    {
+        return mic <= breakpoint.MicBreakpointSusceptible;
+    }
+}
