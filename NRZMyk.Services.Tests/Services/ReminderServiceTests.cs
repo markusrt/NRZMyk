88b87b1633e49861bc6129841856a07f0da7722f
Markus Reinhardt <tuxxilla@gmail.com>
2025-03-13 10:29:25 +0100

    Fix next sending date (#177)
    * Fix next sending date
    * Update  GitHub actions

--- NRZMyk.Services.Tests/Services/ReminderServiceTests.cs
+++ NRZMyk.Services.Tests/Services/ReminderServiceTests.cs
@@ -24,30 +24,58 @@ public class ReminderServiceTests
         sut.HumanReadableExpectedNextSending(org).Should().Be("diesen Monat");
     }
 
-    [TestCase(1, 6, "in 6 Monaten")]
-    [TestCase(2, 6, "in 6 Monaten")]
-    [TestCase(3, 6, "in 6 Monaten")]
-    [TestCase(4, 6, "in 6 Monaten")]
-    [TestCase(5, 6, "in 6 Monaten")]
-    [TestCase(6, 6, "in 6 Monaten")]
-    [TestCase(7, 6, "vor 5 Monaten")]
-    [TestCase(8, 6, "vor 5 Monaten")]
-    [TestCase(19, 6, "vor einem Jahr")]
-    [TestCase(43, 6, "vor 3 Jahren")]
-    [TestCase(10, 2, "in 2 Monaten")]
+    [TestCase(1, 6, "in 5 Monaten")]
+    [TestCase(2, 6, "in 5 Monaten")]
+    [TestCase(3, 6, "in 5 Monaten")]
+    [TestCase(4, 6, "in 5 Monaten")]
+    [TestCase(5, 6, "in 5 Monaten")]
+    [TestCase(6, 6, "in 5 Monaten")]
+    [TestCase(7, 6, "in 5 Monaten")]
+    [TestCase(8, 6, "in 5 Monaten")]
+    [TestCase(19, 6, "vor 6 Monaten")]
+    [TestCase(44, 6, "vor 2 Jahren")]
+    [TestCase(10, 2, "in einem Monat")]
     public void WhenExpectedNextSendingIsChecked_HumanReadableExpectedNextSendingShowsValidInformation(int monthSinceLatestStrainArrival, int monthUntilNextArrival, string expectedNextSending)
     {
         var sut = CreateSut();
         var org = CreateOrganization();
+        var latestCryoDate = DateTime.Today.AddMonths(-1 * monthSinceLatestStrainArrival).AddMonths(1);
         var expectedNextArrival = DateTime.Today.AddMonths(monthUntilNextArrival);
         expectedNextArrival = new DateTime(expectedNextArrival.Year, expectedNextArrival.Month,
             DateTime.DaysInMonth(expectedNextArrival.Year, expectedNextArrival.Month));
         org.DispatchMonth = (MonthToDispatch)expectedNextArrival.Month;
-        org.LatestCryoDate = DateTime.Today.AddMonths(-1 * monthSinceLatestStrainArrival).AddMonths(1);
-        org.LatestCryoDate = new DateTime(org.LatestCryoDate.Value.Year, org.LatestCryoDate.Value.Month, 5);
+        org.LatestCryoDate = new DateTime(latestCryoDate.Year, latestCryoDate.Month, 5);
 
         sut.HumanReadableExpectedNextSending(org).Should().Be(expectedNextSending);
     }
+
+    [Test]
+    public void WhenExpectedNextSendingIsChecked_SentSlightlyEarlyShowsDueNextYear()
+    {
+        var sut = CreateSut();
+        var org = CreateOrganization();
+        var today = DateTime.Today;
+        var twentyDaysAgo = today.Subtract(TimeSpan.FromDays(20));
+        org.DispatchMonth = (MonthToDispatch) today.Month;
+        org.LatestCryoDate = twentyDaysAgo;
+
+        sut.HumanReadableExpectedNextSending(org).Should().Be("in einem Jahr");
+    }
+
+    [Test]
+    public void WhenExpectedNextSendingIsChecked_SentSlightlyLateShowsDueNextYear()
+    {
+        var sut = CreateSut();
+        var org = CreateOrganization();
+        var today = DateTime.Today;
+        var lastMonth = DateTime.Today.AddMonths(-1);
+        var someDaysAgo = today.AddDays(-5);
+        org.DispatchMonth = (MonthToDispatch) lastMonth.Month;
+        org.LatestCryoDate = someDaysAgo;
+
+        sut.HumanReadableExpectedNextSending(org).Should().Be("in 11 Monaten");
+    }
+
 
     [Test]
     public void WhenMonthToDispatchIsNone_HumanReadableExpectedNextSendingShowsNoDispatchMonthSet()
@@ -71,6 +99,19 @@ public class ReminderServiceTests
         sut.CalculateExpectedNextSending(org).Should().BeNull();
     }
 
+    [Test]
+    public void WhenLatestCryoDateIsNull_CalculateExpectedNextSendingReturnsNull()
+    {
+        var sut = CreateSut();
+        var org = CreateOrganization();
+        var today = DateTime.Today.AddMonths(2);
+        org.DispatchMonth = (MonthToDispatch) today.Month;
+        org.LatestCryoDate = null;
+
+        sut.CalculateExpectedNextSending(org).Should().NotBeNull();
+        sut.HumanReadableExpectedNextSending(org).Should().Be(today.Day == 1 ? "in 2 Monaten" : "in einem Monat");
+    }
+
     private static Organization CreateOrganization()
     {
         return new Organization
@@ -82,7 +123,7 @@ public class ReminderServiceTests
         };
     }
 
-    private static ReminderService CreateSut(IEmailNotificationService emailNotificationServiceMock = null)
+    private static ReminderService CreateSut()
     {
         return new ReminderService();
     }
