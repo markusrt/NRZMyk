2e7339245e3b7e301ea026203f678df4f0c3a307
Markus Reinhardt <tuxxilla@gmail.com>
2026-02-15 18:11:32 +0100

    Add proper migration for receiving date (#216)

--- NRZMyk.Services/Data/Migrations/20260215112927_SentinelEntry_AddReceivingDate.cs
+++ NRZMyk.Services/Data/Migrations/20260215112927_SentinelEntry_AddReceivingDate.cs
@@ -1,16 +1,20 @@
-using System;
+﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
 
 namespace NRZMyk.Services.Data.Migrations
 {
+    /// <inheritdoc />
     public partial class SentinelEntry_AddReceivingDate : Migration
     {
+        /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            // Add column as nullable first
             migrationBuilder.AddColumn<DateTime>(
                 name: "ReceivingDate",
                 table: "SentinelEntries",
+                type: "datetime2",
                 nullable: true);
 
             // Initialize ReceivingDate with SamplingDate for existing entries
@@ -25,6 +29,7 @@ namespace NRZMyk.Services.Data.Migrations
                 nullable: false);
         }
 
+        /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropColumn(
